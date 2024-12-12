23a157e27869a3e4fc55a5a031b16047a393a282
Artre0n <adorszhan@gmail.com>
2024-12-12 11:57:42 +0300

    Добавьте файлы проекта.

---
+++ Тумаков - Лабораторная 8 главы/Classes/Bank.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тумаков___Лабораторная_8_главы.Classes
+{
+    public class BankAccount
+    {
+        /// <summary>
+        /// Статическая переменная для генерации уникального номера счета
+        /// </summary>
+        private static int lastAccountNumber = 1;
+
+        /// <summary>
+        /// Закрытые поля
+        /// </summary>
+        private int accountNumber;
+        private decimal balance;
+        private AccountType accountType;
+
+        /// <summary>
+        /// Номер счета
+        /// </summary>
+        public int AccountNumber { get { return accountNumber; } }
+
+        /// <summary>
+        /// Баланс счета
+        /// </summary>
+        public decimal Balance { get { return balance; } }
+
+        /// <summary>
+        /// Тип банковского счета
+        /// </summary>
+        public AccountType AccountType { get { return accountType; } }
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="accountType">Тип банковского счета</param>
+        /// <param name="initialBalance">Начальный баланс</param>
+        public BankAccount(AccountType accountType, decimal initialBalance)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Начальный баланс не может быть отрицательным\n");
+            }
+
+            this.accountNumber = lastAccountNumber++;
+            this.accountType = accountType;
+            this.balance = initialBalance;
+        }
+
+        /// <summary>
+        /// Метод для вывода информации о счёте
+        /// </summary>
+        public void ShowAccountInfo()
+        {
+            Console.WriteLine("Информация о счёте:\n");
+            Console.WriteLine($"Номер счета: {accountNumber}");
+            Console.WriteLine($"Тип счета: {accountType}");
+            Console.WriteLine($"Баланс: {balance}\n");
+        }
+
+        /// <summary>
+        /// Метод для снятия со счета
+        /// </summary>
+        /// <param name="amount">Сумма для снятия</param>
+        /// <returns>Возвращает true, если операция выполнена успешно, иначе false</returns>
+        public bool Withdraw(decimal amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Сумма для снятия не может быть отрицательной\n");
+                return false;
+            }
+
+            if (amount > balance)
+            {
+                Console.WriteLine("Недостаточно средств на счете\n");
+                return false;
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Снято {amount}. Новый баланс: {balance}\n");
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Метод для пополнения счета
+        /// </summary>
+        /// <param name="amount">Сумма для пополнения</param>
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Сумма для пополнения не может быть отрицательной\n");
+                return;
+            }
+
+            balance += amount;
+            Console.WriteLine($"Положено {amount}. Новый баланс: {balance}\n");
+        }
+
+        /// <summary>
+        /// Метод для перевода денег с одного счета на другой
+        /// </summary>
+        /// <param name="paymentAccount">Счёт, с которого переводятся деньги</param>
+        /// <param name="destinationAccount">Счет, на который переводятся деньги</param>
+        /// <param name="amount">Сумма для перевода</param>
+        /// <returns>Возвращает true, если операция выполнена успешно, иначе false</returns>
+        public bool Transfer(BankAccount paymentAccount, BankAccount destinationAccount, decimal amount)
+        {
+            if (destinationAccount == null && destinationAccount is not BankAccount)
+            {
+                Console.WriteLine("Указан недействительный счет назначения\n");
+                return false;
+            }
+
+            if (amount < 0)
+            {
+                Console.WriteLine("Сумма для перевода не может быть отрицательной\n");
+                return false;
+            }
+
+            Console.WriteLine($"Перевод c счёта {paymentAccount.AccountNumber} на счёт {destinationAccount.AccountNumber}:");
+            if (paymentAccount.Withdraw(amount))
+            {
+                destinationAccount.Deposit(amount);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Тумаков - Лабораторная 8 главы/Program.cs
@@ -0,0 +1,107 @@
+﻿using Тумаков___Лабораторная_8_главы;
+using Тумаков___Лабораторная_8_главы.Classes;
+using System.IO;
+
+public class Programm
+{
+    public static void Main()
+    {
+        //Task1();
+        //Task2();
+        Task3();
+    }
+    public static void Task1()
+    {
+        Console.WriteLine("Упражнение 8.1\n");
+        //В класс банковский счет, созданный в упражнениях 7.1 - 7.3 добавить
+        //метод, который переводит деньги с одного счета на другой.У метода два параметра: ссылка
+        //на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.
+
+        BankAccount account1 = new BankAccount(AccountType.Текущий, 1000);
+
+        account1.ShowAccountInfo();
+        account1.Withdraw(500);
+        account1.Deposit(200);
+
+        BankAccount account2 = new BankAccount(AccountType.Накопительный, 500);
+
+        account2.ShowAccountInfo();
+
+        account1.Transfer(account1, account2, 300);
+
+        account1.ShowAccountInfo();
+        account2.ShowAccountInfo();
+    }
+    
+    public static string ReverseString(string input)
+    {
+        try
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        catch(ArgumentNullException)
+        {
+            return $"Входная строка не может быть null";
+        }
+    }
+    public static void Task2()
+    {
+        Console.WriteLine("\nУпражнение 8.2\n");
+        //Реализовать метод, который в качестве входного параметра принимает
+        //строку string, возвращает строку типа string, буквы в которой идут в обратном порядке.
+        //Протестировать метод.
+
+        string testString1 = "It's snowing!";
+        string reversedString1 = ReverseString(testString1);
+        Console.WriteLine($"Исходная строка: {testString1}");
+        Console.WriteLine($"Перевернутая строка: {reversedString1}");
+
+        string testString2 = "12345";
+        string reversedString2 = ReverseString(testString2);
+        Console.WriteLine($"Исходная строка: {testString2}");
+        Console.WriteLine($"Перевернутая строка: {reversedString2}");
+    }
+    public static void Task3()
+    {
+        Console.WriteLine("\nУпражнение 8.3\n");
+        //Написать программу, которая спрашивает у пользователя имя файла. Если
+        //такого файла не существует, то программа выдает пользователю сообщение и заканчивает
+        //работу, иначе в выходной файл записывается содержимое исходного файла, но заглавными
+        //буквами.
+
+        Console.Write("Введите имя файла: ");
+        string fileName = "text.txt";
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string[] files = Directory.GetFiles(baseDirectory, "*.txt");
+        foreach (string file in files)
+        {
+            if (!File.Exists(Path.GetFileName(file)))
+            {
+                Console.WriteLine("Файл не существует.");
+                return;
+            }
+        }
+        
+        try
+        {
+            string text = File.ReadAllText(fileName);
+            string upperText = text.ToUpper();
+
+            string newFileName = Path.GetFileNameWithoutExtension(fileName) + "_upper" + Path.GetExtension(fileName);
+            File.WriteAllText(newFileName, upperText);
+
+            Console.WriteLine("Содержимое файла записано в файл " + newFileName + " в верхнем регистре.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
