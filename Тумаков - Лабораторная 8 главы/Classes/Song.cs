122c6151ba43d73d989cc06e117ae01899b539df
Artre0n <adorszhan@gmail.com>
2024-12-12 19:34:27 +0300

    #

---
+++ Тумаков - Лабораторная 8 главы/Classes/Song.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тумаков___Лабораторная_8_главы.Classes
+{
+    public class Song
+    {
+        private string name; // название песни
+        private string author; // автор песни
+        private Song prev; // связь с предыдущей песней в списке
+
+        /// <summary>
+        /// Метод для заполнения поля name
+        /// </summary>
+        /// <param name="name">Название песни</param>
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Метод для заполнения поля author
+        /// </summary>
+        /// <param name="author">Автор песни</param>
+        public void SetAuthor(string author)
+        {
+            this.author = author;
+        }
+
+        /// <summary>
+        /// Метод для заполнения поля prev
+        /// </summary>
+        /// <param name="prev">Предыдущая песня</param>
+        public void SetPrev(Song prev)
+        {
+            this.prev = prev;
+        }
+
+        /// <summary>
+        /// Метод для печати названия песни и ее исполнителя
+        /// </summary>
+        public void Title()
+        {
+            Console.WriteLine($"Песня: {name}, Исполнитель: {author}");
+        }
+
+        /// <summary>
+        /// Метод, который сравнивает две песни
+        /// </summary>
+        /// <param name="obj">Объект для сравнения</param>
+        /// <returns>True, если песни одинаковые; иначе False</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Song otherSong)
+            {
+                return this.name == otherSong.name && this.author == otherSong.author;
+            }
+            return false;
+        }
+    }
+}
--- Тумаков - Лабораторная 8 главы/Program.cs
+++ Тумаков - Лабораторная 8 главы/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Тумаков___Лабораторная_8_главы;
 using Тумаков___Лабораторная_8_главы.Classes;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 public class Programm
 {
@@ -8,7 +9,10 @@ public class Programm
     {
         //Task1();
         //Task2();
-        Task3();
+        //Task3();
+        //Task4();
+        //Task5();
+        Task6();
     }
     public static void Task1()
     {
@@ -76,7 +80,6 @@ public class Programm
         //работу, иначе в выходной файл записывается содержимое исходного файла, но заглавными
         //буквами.
 
-        Console.Write("Введите имя файла: ");
         string fileName = "text.txt";
         string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
         string[] files = Directory.GetFiles(baseDirectory, "*.txt");
@@ -102,6 +105,165 @@ public class Programm
         catch (Exception ex)
         {
             Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+    static bool CheckIfIFormattable(object obj)
+    {
+        if (obj is IFormattable)
+        {
+            return true;
+        }
+        IFormattable formattable = obj as IFormattable;
+        return formattable != null;
+    }
+    public static void Task4()
+    {
+        Console.WriteLine("\nУпражнение 8.4\n");
+        //Реализовать метод, который проверяет реализует ли входной параметр
+        //метода интерфейс System.IFormattable.Использовать оператор is и as. (Интерфейс
+        //IFormattable обеспечивает функциональные возможности форматирования значения объекта
+        //в строковое представление.)
+
+        object obj1 = 123;
+        object obj2 = "Hello";
+        object obj3 = 45.67; 
+        Console.WriteLine(CheckIfIFormattable(obj1)); 
+        Console.WriteLine(CheckIfIFormattable(obj2)); 
+        Console.WriteLine(CheckIfIFormattable(obj3));
+    }
+    public static void SearchMail(ref string s)
+    {
+        string[] parts = s.Split('#');
+        if (parts.Length > 1)
+        {
+            s = parts[1].Trim();
+        }
+        else
+        {
+            s = string.Empty;
+        }
+    }
+    public static void Task5()
+    {
+        Console.WriteLine("Домашнее задание 8.1");
+        //Работа со строками. Дан текстовый файл, содержащий ФИО и e-mail
+        //адрес.Разделителем между ФИО и адресом электронной почты является символ #:
+        //Иванов Иван Иванович # iviviv@mail.ru
+        //Петров Петр Петрович # petr@mail.ru
+        //Сформировать новый файл, содержащий список адресов электронной почты.
+        //Предусмотреть метод, выделяющий из строки адрес почты. Методу в
+        //качестве параметра передается символьная строка s, e-mail возвращается в той же строке s:
+
+        string inputFilePath = "input.txt"; 
+        string outputFilePath = "output.txt";
+
+        if (!File.Exists(inputFilePath))
+        {
+            Console.WriteLine("Входной файл не найден.");
+            return;
+        }
+
+        bool emailFound = false;
+        foreach (var line in File.ReadLines(inputFilePath))
+        {
+            string email = line;
+            SearchMail(ref email); 
+            if (!string.IsNullOrEmpty(email))
+            {
+                emailFound = true; 
+        
+                using (StreamWriter writer = new StreamWriter(outputFilePath, true))
+                {
+                    writer.WriteLine(email);
+                }
+            }
+        }
+
+        if (emailFound)
+        {
+            Console.WriteLine("Адреса электронной почты успешно сохранены в файл " + outputFilePath);
+        }
+        else
+        {
+            Console.WriteLine("Адреса электронной почты не найдены.");
+        }
+    }
+    public static void Task6()
+    {
+        Console.WriteLine("\nДомашнее задание 8.2\n");
+        //Список песен. В методе Main создать список из четырех песен. В
+        //цикле вывести информацию о каждой песне. Сравнить между собой первую и вторую
+        //песню в списке.Песня представляет собой класс с методами для заполнения каждого из
+        //полей, методом вывода данных о песне на печать, методом, который сравнивает между
+        //собой два объекта
+        // Пример:
+        // Селфхарм - Монеточка
+        // Моя невеста - Егор Крид
+        // В белом невесте - 4k
+        // Tramp - Otis Redding
+        // Заходи - Амбисаша
+        try
+        {
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Song song = new Song();
+                string songName, songAuthor;
+
+                Console.Write($"Введите название песни {i + 1}: ");
+                songName = (Console.ReadLine());
+
+                if (string.IsNullOrWhiteSpace(songName))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                Console.Write($"Введите автора песни {i + 1}: ");
+                songAuthor = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(songAuthor))
+                {
+                    throw new ArgumentException();
+                }
+
+                song.SetName(songName);
+                song.SetAuthor(songAuthor);
+
+                if (i > 0)
+                {
+                    song.SetPrev(songs[i - 1]);
+                }
+
+                songs.Add(song);
+            }
+
+            Console.WriteLine("\nИнформация о песнях:");
+            foreach (var song in songs)
+            {
+                song.Title();
+            }
+
+            if (songs.Count >= 2)
+            {
+                bool areEqual = songs[0].Equals(songs[1]);
+                if (areEqual)
+                {
+                    Console.WriteLine("\nПервая и вторая песни одинаковые.");
+                }
+                else
+                {
+                    Console.WriteLine("\nПервая и вторая песни разные.");
+                }
+            }
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Ошибка: Название песни не может быть пустым. Попробуйте снова");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Ошибка: Автор песни не может быть пустым. Попробуйте снова");
         }
     }
 }
